0b7261a7107d2c2f9176bfdce8f1a0c3e05133e9
FaxanaduHacks <123332890+FaxanaduHacks@users.noreply.github.com>
2023-08-05 04:07:51 -0700

    Update dice_recognizer.py
    Moving comments to the README.

--- dice_recognizer.py
+++ dice_recognizer.py
@@ -1,35 +1,4 @@
 #!/usr/bin/env python3
-################################################################################
-# File Name:    dice-recognizer.py
-# Version:      0.1
-# Created:      2023-06-30
-# Created By:   Faxanadu
-# Modified:     2023-06-30
-# Modified By:  Faxanadu
-# Env Notes:    vi: set expandtab tabstop=4 shiftwidth=4 softtabstop=4
-# Synopsis:     This script recognizes 6 sided dice with pips, either a single
-#               die or multiple dice, counts the number of pips on each dice
-#               face and displays the total number rolled. It is built to
-#               account for variable lighting if the camera has the ability to
-#               focus and applies some basic preprocessing on the frame by
-#               first gray scale the image, then applie a Gaussian blur to
-#               improve the thresholding and contours. The bounding boxes are
-#               actually used to detect the dice frames and later the bounding
-#               circles for visual representation of what was actually
-#               detected. This script seems to work well with various dice of
-#               multiple colors and sizes (including translucent dice that
-#               throw off weird reflections). There is a second window for
-#               settings and trackbars to adjust the settings in real time to
-#               aid with calibration in various lighting environments. I keep
-#               it around 0.8 or 0.9 depending on the lighting. The default
-#               threshold for circularity is 0.6. Note: the detection happens
-#               with rectangular bounding boxes because it's more accurate, the
-#               circular bounding boxes are drawn to show what was detected to
-#               the user and should be on the edges of the pips. The program
-#               was tested with dice that have white pips. Press q to quit.
-# Changelog:    Note - Stack comments oldest to newest.
-#               + 2023-06-30 - File created. -Faxanadu
-################################################################################
 
 import cv2
 import numpy as np
